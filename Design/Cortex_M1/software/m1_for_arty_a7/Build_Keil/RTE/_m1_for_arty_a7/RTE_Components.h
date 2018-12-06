62861ae386827f4503bb557ece97eef1dcac2354
Peter <43818041+PeterSchober@users.noreply.github.com>
2018-12-06 19:27:15 +0100

    Added SysTick
    Internal Interrupt now triggers the LED's and toggles them

---
+++ Design/Cortex_M1/software/m1_for_arty_a7/Build_Keil/RTE/_m1_for_arty_a7/RTE_Components.h
@@ -0,0 +1,20 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'm1_for_arty_a7' 
+ * Target:  'm1_for_arty_a7' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "ARMCM1.h"
+
+
+#endif /* RTE_COMPONENTS_H */
--- Design/Cortex_M1/software/m1_for_arty_a7/main/main.c
+++ Design/Cortex_M1/software/m1_for_arty_a7/main/main.c
@@ -10,7 +10,6 @@
  * by a licensing agreement from ARM Limited.
  * ----------------------------------------------------------------
  * File:     main.c
- * Release Information : Cortex-M1 DesignStart-r0p0-00rel0
  * ----------------------------------------------------------------
  *
  */
@@ -34,26 +33,37 @@
 
 /*******************************************************************/
 
+uint8_t LED_val = 0; // Value will be toggled for making the LED's Blink
+XGpio myGpio_LED; // GPIO IP Control
+
+void SysTick_Handler(void)  {  /* SysTick interrupt Handler. */
+	XGpio_DiscreteWrite(&myGpio_LED, 1, LED_val); // void XGpio_DiscreteWrite(XGpio *InstancePtr, unsigned Channel, u32 Mask);
+	LED_val = ~LED_val; // bitwise NOT
+}
+
 int main (void)
 {
-	// GPIO LED
-	int status_LED;
-	XGpio myGpio_LED;
-	long long output; 
-	output = 4294967295;
-            
-	status_LED = XGpio_Initialize(&myGpio_LED,XPAR_GPIO_0_DEVICE_ID);
+	uint32_t status_sysTick;
+	uint32_t status_LED;
+
+
+// Configure System Tick Timer	
+	status_sysTick = SysTick_Config(SystemCoreClock); // Change the frequency of SysTick interrupts
+	
+	status_LED = XGpio_Initialize(&myGpio_LED,XPAR_GPIO_0_DEVICE_ID);// Required Initialization
 	if (status_LED != XST_SUCCESS) {
 		return XST_FAILURE;
 	}
 	
-	XGpio_SetDataDirection(&myGpio_LED, 1,0x00); // all are outputs //Second Parameter is GPIO Channel
-	XGpio_DiscreteWrite(&myGpio_LED, 1, 3); // void XGpio_DiscreteWrite(XGpio *InstancePtr, unsigned Channel, u32 Mask);
-		
+	XGpio_SetDataDirection(&myGpio_LED, 1,0x00); // all GPIO's are outputsSecond Parameter is GPIO Channel
     
     while ( 1 )
     {
-				
     
     }
 }
+
+// Configure System Tick Timer with Registers Cortex-M1 Specific
+//  volatile u32 *pSystick_CSR = (u32 *)0xE000E010; // SysTick Control and Status Register
+//	volatile u32 *pSystick_RVR = (u32 *)0xE000E014; // SysTick Reload Value Register
+//	volatile u32 *pSystick_CVR = (u32 *)0xE000E018; // SysTick Current Value Register
